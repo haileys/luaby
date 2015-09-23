076fecd2158fc0503f8e47e62555378faf3461e7
Jack Chen (chendo) <github@chen.do>
2015-09-23 20:25:56 +1000

    Don't use ... when not varadic

--- lib/luaby/ast/parameters.rb
+++ lib/luaby/ast/parameters.rb
@@ -1,21 +1,21 @@
 class Luaby::AST::Parameters
   attr_accessor :variadic, :named_args
-  
+
   def initialize(variadic, named_args)
     @variadic = variadic
     @named_args = named_args
   end
-  
+
   def variadic?
     variadic
   end
-  
+
   def to_lua
     if named_args.any?
       str = "#{named_args.join(", ")}"
       str << ", ..." if variadic?
     else
-      "..."
+      ""
     end
   end
 end
